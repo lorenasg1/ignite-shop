b30e2bc970a98e92cefc7254d7245fd9a9d67b7f
Lorena <lorena@pop-os.localdomain>
2023-01-10 09:23:04 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image"
 import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import shirt1 from '../assets/1.png'
+import shirt2 from '../assets/2.png'
 
 const Button = styled('button', {
   backgroundColor: "$green300",
@@ -17,9 +22,24 @@ const Button = styled('button', {
 
 export default function Home() {
   return (
-    <Button>
-      <span>hello</span>
-      world
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="camiseta 1" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="camiseta 1" />
+
+        <footer>
+          <strong>Camiseta y</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
 import { createStitches } from "@stitches/react";
 
-export const {config, styled, globalCss, keyframes, getCssText, theme, createTheme, css} = createStitches({
+export const { config, styled, globalCss, keyframes, getCssText, theme, createTheme, css } = createStitches({
   theme: {
     colors: {
       white: '#fff',
@@ -12,6 +12,12 @@ export const {config, styled, globalCss, keyframes, getCssText, theme, createThe
 
       green500: '#00875f',
       green300: '#00b37e',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
     }
   }
 })
